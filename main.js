0f485e14dfe104d577d0085f02a37f74fa52e438
AbdielMamani <abdielmamani@upeu.edu.pe>
2023-12-05 18:10:54 -0500

    Adding styles and content-security-policy

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 const path = require('path');
 
 const {app, BrowserWindow} = require('electron');
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'development';
 //Window app
 function createMainWindow(){
  const mainWindow= new BrowserWindow({
     title: 'Image Resizer',
-    width: 500,
+    width: isDev? 1000: 500,
     height: 600
  });   
+
+ //open devtools if in dev env
+
+ if(isDev){
+    mainWindow.webContents.openDevTools();
+ }
+
+
  mainWindow.loadFile(path.join(__dirname, './render/index.html'));
 }  
 
-
+//When the app is going to run 
 app.whenReady().then(()=>{
     createMainWindow();
+
+    app.on('activate',()=>{
+        if(BrowserWindow.getAllWindows().length === 0){
+            createMainWindow()
+        }
+    });
 })
+
+app.on('window-all-closed', ()=>{
+    if(isMac){
+        app.quit()
+    }
+});

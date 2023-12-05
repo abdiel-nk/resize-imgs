1351c3f8fb517d5be808667446fb04f5e4e4cfcd
AbdielMamani <abdielmamani@upeu.edu.pe>
2023-12-05 18:19:41 -0500

    origin

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const isMac = process.platform === 'darwin';
 const isDev = process.env.NODE_ENV !== 'development';
-//Window app
+
+//Create main Window 
 function createMainWindow(){
  const mainWindow= new BrowserWindow({
     title: 'Image Resizer',
@@ -25,12 +26,29 @@ function createMainWindow(){
 app.whenReady().then(()=>{
     createMainWindow();
 
+    //implement menu
+
+    const mainMenu= Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu)
+
     app.on('activate',()=>{
         if(BrowserWindow.getAllWindows().length === 0){
             createMainWindow()
         }
     });
 })
+//Menu template
+const menu= {
+    label:'File',
+    submenu:[{
+        label: 'Quit',
+        click: ()=>app.quit(),
+        accelerator: 'CmdOrCtrl+W'
+    }
+   ]
+};
+
+
 
 app.on('window-all-closed', ()=>{
     if(isMac){
